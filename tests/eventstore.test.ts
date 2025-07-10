7122ca7b344e2006662e8cd88956683df6644c59
Ralf Westphal <info@ralfw.de>
2025-07-10 12:14:14 +0300

    bug fix: db init läuft jetzt

--- tests/eventstore.test.ts
+++ tests/eventstore.test.ts
@@ -23,11 +23,11 @@ class TestEvent implements IHasEventType {
 describe('EventStore', () => {
   let eventStore: PostgresEventStore;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     eventStore= new PostgresEventStore(
       { connectionString: process.env.DATABASE_TEST_URL || 'postgres://postgres:postgres@localhost:5432/eventstore_test' }
     );
-    eventStore.initializeDatabase();
+    await eventStore.initializeDatabase();
   });
 
   afterEach(async () => {
