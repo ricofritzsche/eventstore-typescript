268d93f9bd84550c7e10b1af09ca0f1fca5927ea
Ralf Westphal <info@ralfw.de>
2025-07-24 15:48:35 +0300

    exporting MemoryEventStore

--- src/eventstore/index.ts
+++ src/eventstore/index.ts
@@ -1,4 +1,7 @@
 export { PostgresEventStore, PostgresEventStoreOptions } from './stores/postgres';
+export { MemoryEventStore } from './stores/memory';
+
 export { MemoryEventStreamNotifier } from './notifiers';
+
 export { createFilter } from './filter';
 export * from './types';
